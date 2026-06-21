550cb09b108b8cb437146f9dc957c1b3707f38c4
FirstPrizeGames <ab38176970@gmail.com>
2026-06-21 20:28:08 +0900

    Add files via upload

--- 2026-05-22/ui/sw.js
+++ 2026-05-22/ui/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "profile-offline-20260621-account-no-logout";
+const CACHE_NAME = "profile-offline-20260621-profile-photo-center";
 const OFFLINE_URL = "/offline";
 const PRECACHE_URLS = [
   OFFLINE_URL,
